ac75aff63de7df1890cda6096d20a23413ff4bc5
sjoon076@gmail.com <sjoon076@gmail.com>
2025-10-17 16:42:36 +0200

    Javascript API Fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,18 @@ fetch("./assets/js/data.json")
   .then((reponse) => reponse.json())
   .then((data) => {
     console.log(data);
+    let outputHtml = "";
+    data.results.forEach((user) => {
+      outputHtml += `
+        <figure class="user">
+               <h4>${user.name.first} ${user.name.last}</h4>
+               <img src="${user.picture.thumbnail}" alt="${user.name.first} ${user.name.last}">
+               
+             <figcaption>${user.email}</figcaption>
+         </figure>
+               `;
+    });
+    document.getElementById("users").innerHTML = outputHtml;
   })
   .catch((error) => {
     console.log(error);
